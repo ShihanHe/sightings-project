73cfd8f5b143dfe1d960b98d99f2e10303d44fb5
Shihan <sh4087@columbia.edu>
2020-10-14 15:10:34 +0000

    Add commands

--- project/urls.py
+++ project/urls.py
@@ -18,8 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     
-    path('map/', include('map.urls'),
-    path('sightings/', include('sightings.urls'),
+   # path('map/', include('map.urls')),
+    path('sightings/', include('sightings.urls')),
     path('admin/', admin.site.urls),
 
-]
+ ]
--- sightings/management/commands/import_squirrel_data.py
+++ sightings/management/commands/import_squirrel_data.py
@@ -1,7 +1,11 @@
 import csv
 import re
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
+from dateutil.parser import parser
+from sightings.models import Squirrel
+from datetime import date
+
 
 class Command(BaseCommand):
     help = 'Import the csv to database'
@@ -9,7 +13,47 @@ class Command(BaseCommand):
     def add_arguments(self,parser):
         parser.add_argument('path', type=str)
 
-    def handle(self, *args, **kargs):
+    def handle(self, *args, **kwargs):
+       
+        path = kwargs['path']
+        
+        pattern = re.compile(r'(d{2})(\d{2})(\d{4})')
 
+        with open(path, 'r') as csv_file:
+            
+            reader = csv.reader(csv_file, dialect='excel')
+            
+            header = reader.next()
+            
+            for row in reader:
+                
+                month, day, year = pattern.match(row[5]).groups()
+                
+                squirrel, created = Squirrel.object.get_or_create(
+                   
+                    Latitude = float(row[0]),
+                    Longitude = float(row[1]),
+                    Unique_Squirrel_ID = row[2],
+                    Shift = row[4],
+                    Date = date(int(year), int(month), int(day)),
+                    Age = row[7], 
+                    Primary_Fur_Color = row[8],
+                    Location = row[12],
+                    Specific_Location = True if row[14] == 'TRUE' else False,
+                    Running = True if row[15] == 'TRUE' else False,
+                    Chasing = True if row[16] == 'TRUE' else False,
+                    Climbing = True if row[17] == 'TRUE' else False,
+                    Eating = True if row[18] == 'TRUE' else False,
+                    Foraging = True if row[19] == 'TRUE' else False,
+                    Other_Activities = True if row[20] == 'TRUE' else False,
+                    Kuks = True if row[21] == 'TRUE' else False,
+                    Quaas = True if row[22] == 'TRUE' else False,
+                    Moans = True if row[23] == 'TRUE' else False,
+                    Tail_Flags = True if row[24] == 'TRUE' else False,
+                    Tail_Twitches = True if row[25] == 'TRUE' else False,
+                    Approaches = True if row[26] == 'TRUE' else False,
+                    Indifferent = True if row[27] == 'TRUE' else False,
+                    Runs_From = True if row[28] == 'TRUE' else False
 
-
+                
+            )
