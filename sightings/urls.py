61f96f9df1ce7b6128dfa82f012cf7994941bec4
xc2554 <xc2554@columbia.edu>
2020-10-21 21:51:57 +0000

    Modify the regex of Unique_Squirrel_ID

--- sightings/urls.py
+++ sightings/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
+
 
 app_name = 'sightings'
 
 urlpatterns = [
         path('', views.list_of_squirrels, name='list_of_squirrels'),
-        path('<str:squirrel_id>/', views.update_squirrel, name='update_squirrel'),
+        re_path(r'^.*([1-9].*[1-9]).*$', views.detail, name='detail'),
         path('create/', views.create_squirrel, name='create_squirrel'),
         path('stats/', views.stats, name='stats'),
 ]
