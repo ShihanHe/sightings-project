e6014a0c11b359e936aebb4d67168975f13ffc20
Shihan <sh4087@columbia.edu>
2020-10-21 18:18:21 +0000

    add csv

--- sightings/management/commands/import_squirrel_data.py
+++ sightings/management/commands/import_squirrel_data.py
@@ -14,22 +14,19 @@ class Command(BaseCommand):
         parser.add_argument('path', type=str)
 
     def handle(self, *args, **kwargs):
-       
         path = kwargs['path']
-        
-        pattern = re.compile(r'(d{2})(\d{2})(\d{4})')
-
-        with open(path, 'r') as csv_file:
+        pattern = re.compile(r'(\d{2})(\d{2})(\d{4})') 
+        with open(path, 'r', encoding='utf-8') as csv_file:
             
             reader = csv.reader(csv_file, dialect='excel')
             
-            header = reader.next()
-            
+            next(reader)
+            USID = set()
             for row in reader:
-                
-                month, day, year = pattern.match(row[5]).groups()
-                
-                squirrel, created = Squirrel.object.get_or_create(
+                if row[2] not in USID:
+                    month, day, year = pattern.match(row[5]).groups()
+               
+                    _,created = Squirrel.objects.get_or_create(
                    
                     Latitude = float(row[0]),
                     Longitude = float(row[1]),
@@ -45,15 +42,16 @@ class Command(BaseCommand):
                     Climbing = True if row[17] == 'TRUE' else False,
                     Eating = True if row[18] == 'TRUE' else False,
                     Foraging = True if row[19] == 'TRUE' else False,
-                    Other_Activities = True if row[20] == 'TRUE' else False,
+                    Other_Activitites = True if row[20] == 'TRUE' else False,
                     Kuks = True if row[21] == 'TRUE' else False,
                     Quaas = True if row[22] == 'TRUE' else False,
                     Moans = True if row[23] == 'TRUE' else False,
-                    Tail_Flags = True if row[24] == 'TRUE' else False,
-                    Tail_Twitches = True if row[25] == 'TRUE' else False,
+                    Tail_flags = True if row[24] == 'TRUE' else False,
+                    Tail_twitches = True if row[25] == 'TRUE' else False,
                     Approaches = True if row[26] == 'TRUE' else False,
                     Indifferent = True if row[27] == 'TRUE' else False,
-                    Runs_From = True if row[28] == 'TRUE' else False
+                    Runs_from = True if row[28] == 'TRUE' else False
 
                 
-            )
+                    )
+                    USID.add(row[2])
