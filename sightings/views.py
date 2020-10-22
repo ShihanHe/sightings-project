cb81d85d758efa2878edea7f1407a645e0940af1
xc2554 <xc2554@columbia.edu>
2020-10-22 08:49:39 +0000

    Modify update.html

--- sightings/views.py
+++ sightings/views.py
@@ -16,19 +16,21 @@ def list_of_squirrels(request):
 
 def update(request, Unique_Squirrel_ID):
     squirrel = get_object_or_404(Squirrel, Unique_Squirrel_ID=Unique_Squirrel_ID)
-
-    context = {
-            'squrriel': squirrel,
-            'ID': Unique_Squirrel_ID,
+    if request.method == 'POST':
+        form = SquirrelForm(request.POST, instance=squirrel)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/sightings/')
+    else:
+        form = SquirrelForm(instance=squirrel)
+    
+    context ={
+        'form':form,
+        'sqid':Unique_Squirrel_ID,
     }
 
     return render(request, 'sightings/update.html', context)
 
-
-def update_squirrel(request):
-
-    
-    return render(request, 'sightings/update.html', context)
 
 def create_squirrel(request):
     if request.method == "POST":
